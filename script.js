1f847b9876f7a0f3ad7e2377ad68b06c9583d31c
Giancarlo Zuliani <slaigox@gmail.com>
2020-11-05 17:50:12 +0100

    script added

---
+++ script.js
@@ -0,0 +1,34 @@
+
+var randomNum=[];
+var userPick = [];
+var rightNum=[];
+
+//genarete number
+
+function generateNumbers(){
+  for(i=0 ; i < 5 ; i++){
+    let n = Math.floor(Math.random() * 100);
+    randomNum.includes(n) ? i-- : randomNum.push(n);
+  }
+  alert(randomNum)
+}
+
+
+generateNumbers();
+
+
+setTimeout(function(){
+  while(userPick.length < 5){
+    let n = parseInt(prompt('inserisci un numero'));
+    userPick.includes(n) ? alert('numero gia inserito') : userPick.push(n);
+  }
+  let point = 0;
+  for(i=0; i < userPick.length ; i++){
+      if(randomNum.includes(userPick[i])){
+        rightNum.push(userPick[i])
+        point++;
+      }
+    }
+  console.log('hai indovinato questi numeri ' + rightNum);
+  console.log('hai totalizzato ' + point + ' punti su 5' )
+},3000)
